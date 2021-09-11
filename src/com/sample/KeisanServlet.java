fc7cb4a241a202ccde7ced56318cf12cb02d182f
1noseA <1noseA2L@gmail.com>
2021-09-12 00:11:04 +0900

    計算課題(request)

--- src/com/sample/KeisanServlet.java
+++ src/com/sample/KeisanServlet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloServlet
+ * Servlet implementation class KeisanServlet
  */
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/Keisan")
+public class KeisanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloServlet() {
+    public KeisanServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,20 +28,18 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// doPostに書いてもいい。そしたらdoGetメソッドは別のもの書ける
-		String name = request.getParameter("name");
-		String password = request.getParameter("password");
-		System.out.println(password);
+		int number1 = Integer.parseInt(request.getParameter("number1"));
+		int number2 = Integer.parseInt(request.getParameter("number2"));
+		int result = number1 + number2;
 
 		PrintWriter out = response.getWriter();
-		out.println("<html><body><h1>hello" + name + "</h1></body></html>");
+		out.println("<html><body><h1>" + result + "</h1></body></html>");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
